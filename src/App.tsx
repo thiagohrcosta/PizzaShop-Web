8537c23d95d72f6e9eb0a00b211542750a5dc672
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-08-27 16:38:32 -0300

    Adding React Helmt SEO

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
 import './global.css'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 export function App() {
   return (
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
 
