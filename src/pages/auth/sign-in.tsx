61a557f9bbac3fa2c05ad6124f2d735df19ce3bf
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-09-19 20:23:08 -0300

    Create restaurant

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -7,7 +7,7 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { toast } from "sonner"
 
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 import { useMutation } from "@tanstack/react-query"
 import { signIn } from "@/api/sign-in"
 
@@ -18,7 +18,13 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
+  const [searchParams] = useSearchParams()
+
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>({
+    defaultValues: {
+      email: searchParams.get('email') ?? '',
+    }
+  })
 
   const { mutateAsync: authenticate } = useMutation({
     mutationFn: signIn,
