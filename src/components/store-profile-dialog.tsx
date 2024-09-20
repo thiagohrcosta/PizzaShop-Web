778ab8ec478d65a90144db6b101dfba440f36555
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-09-20 18:27:27 -0300

    Update profile information

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,32 +1,87 @@
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { Button } from "./ui/button";
 import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from "zod";
+import { updateProfile } from "@/api/update-profile";
+import { toast } from "sonner";
+import { DialogClose } from "@radix-ui/react-dialog";
+
+const storeProfileSchmea = z.object({
+  name: z.string().min(1),
+  description: z.string()
+})
+
+type StoreProfileSchema = z.infer<typeof storeProfileSchmea>
 
 export function StoreProfileDialog() {
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<StoreProfileSchema>({
+    resolver: zodResolver(storeProfileSchmea),
+    values: {
+      name: managedRestaurant?.name ?? '',
+      description: managedRestaurant?.description ?? ''
+    },
+  })
+
+  const { mutateAsync: updateProfileFn } = useMutation({
+    mutationFn: updateProfile
+  })
+
+  async function handleUpdateProfile(data: StoreProfileSchema) {
+    try {
+      await updateProfile({
+        name: data.name,
+        description: data.description
+      })
+
+      toast.success('Profile updated with successfully"')
+    } catch {
+      toast.error('Failure to update profile, try it again"')
+    }
+  }
+
   return (
     <DialogContent>
       <DialogHeader>
-        <DialogTitle>Store Profile</DialogTitle>
+        <DialogTitle>Profile</DialogTitle>
         <DialogDescription>Update your store information visible to your customer</DialogDescription>
       </DialogHeader>
 
-      <form>
+      <form onSubmit={handleSubmit(handleUpdateProfile)}>
         <div className="space-y-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="name">Name</Label>
-            <Input className="col-span-3" id="name" />
+            <Input className="col-span-3" id="name" {...register('name')} />
           </div>
 
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="description">Description</Label>
-            <Textarea className="col-span-3" id="description" />
+            <Textarea className="col-span-3" id="description" {...register('description')} />
           </div>
         </div>
         <DialogFooter>
-          <Button variant="ghost" type="button">Cancel</Button>
-          <Button type="submit" variant="success">Save</Button>
+          <DialogClose asChild>
+            <Button variant="ghost" type="button">
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button type="submit" variant="success" disabled={isSubmitting}>
+            Save
+          </Button>
         </DialogFooter>
       </form>
       
