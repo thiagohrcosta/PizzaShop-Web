793d77b95d4adc2f954bb4ee2e579919e3aa4337
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-10-25 17:21:22 -0300

    Adding data from backend

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -1,7 +1,14 @@
+import { getDayOrdersAmount } from "@/api/get-day-orders-amount";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
 import { Utensils } from "lucide-react";
 
 export function DayOrdersAmountCard() {
+  const { data: dayOrdersAmount } = useQuery({
+    queryFn: getDayOrdersAmount,
+    queryKey: ['metrics', 'day-orders-amount']
+  })
+  
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -9,13 +16,32 @@ export function DayOrdersAmountCard() {
         <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">
-          12
-        </span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text=rose-500 dark:text-rose-400">-4%</span> based on previously month
-        </p>
-
+        {dayOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {dayOrdersAmount.amount || 0}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {dayOrdersAmount.diffFromYesterday >= 0 ? (
+                <>
+                  <span className="text=rose-500 dark:text-emerald-400">
+                    +{dayOrdersAmount.diffFromYesterday}%
+                  </span>{' '} 
+                  based yesterday
+                </>
+                
+              ) : (
+                <>
+                  <span className="text=rose-500 dark:text-rose-400">
+                    -{dayOrdersAmount.diffFromYesterday}%
+                  </span>{' '} 
+                  based on yesterday
+                </>
+                
+              )}
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
--- src/pages/app/dashboard/month-revenue-card.tsx
+++ src/pages/app/dashboard/month-revenue-card.tsx
@@ -1,7 +1,14 @@
+import { getMonthRevenue } from "@/api/get-month-revenue";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
 import { DollarSign } from "lucide-react";
 
 export function MonthRevenueCard() {
+  const { data: monthRevenue } = useQuery({
+    queryFn: getMonthRevenue,
+    queryKey: ['metrics', 'day-orders-amount']
+  })
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -9,13 +16,36 @@ export function MonthRevenueCard() {
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">
-          $ 1,200.99
-        </span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text=emerald-500 dark:text-emerald-400">+2%</span> based on previously month
-        </p>
-
+        {monthRevenue && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthRevenue?.receipt != null 
+                ?(monthRevenue?.receipt / 100).toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD'
+                  })
+                : 0
+              }
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthRevenue.diffFromLastMonth >= 0 ? (
+                <>
+                  <span className="text=rose-500 dark:text-emerald-400">
+                    +{monthRevenue.diffFromLastMonth}%
+                  </span>{' '} 
+                  based previously month
+                </>
+              ) : (
+                <>
+                  <span className="text=rose-500 dark:text-rose-400">
+                    -{monthRevenue.diffFromLastMonth}%
+                  </span>{' '} 
+                  based previously month
+                </>
+              )}
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
