75a9e9039443fbf69df3ba27c0d3f5d821cc9e74
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-11-05 20:13:35 -0300

    profile and restaurant mock

--- src/api/get-managed-restaurant.ts
+++ src/api/get-managed-restaurant.ts
@@ -3,12 +3,10 @@ import { api } from "@/lib/axios";
 export interface GetManageRestaurantResponse {
   id: string
   name: string
-  email: string
-  description: string
-  phone: string | null
-  role: 'manager' | 'cutomer'
   createdAt: Date | null
   updatedAt: Date | null
+  description: string | null
+  managerId: string | null
 }
 export async function getManagedRestaurant() {
   const response = await api.get<GetManageRestaurantResponse>('/managed-restaurant')
