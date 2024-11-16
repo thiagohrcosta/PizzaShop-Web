9c565e9ee7a3d550bff69a6af4e9a361e4665d2b
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-11-16 18:09:22 -0300

    Adding background image

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,16 +1,27 @@
 import { Pizza } from "lucide-react";
 import { Outlet } from "react-router-dom";
 
+import PizzaBackend from "../../assets/pizza.png";
+
 export function AuthLayout() {
   return (
     <div className="min-h-screen grid grid-cols-2 antialiased">
-      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+      <div 
+        className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between"
+        style={{
+          backgroundImage: `url(${PizzaBackend})`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          boxShadow: 'inset 0 0 0 2000px rgba(0, 0, 0, 0.5)',
+        }}  
+      >
         <div className="flex items-center gap-3 text-lg font-medium text-foreground">
           <Pizza className="h-5 w-5">
             <span className="font-semibold">pizza.ship</span>
           </Pizza>
         </div>
-        <footer className="text-sm">
+        <footer className="text-sm text-white">
           Partner Dashboard &copy; pizza.shop - { new Date().getFullYear() }
         </footer>
       </div>
